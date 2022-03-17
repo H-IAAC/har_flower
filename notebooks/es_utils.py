6452d1e2bc777f6b94d22a92bb918e33f5d7fe78
Wandemberg Gibaut <wandemberg.gibaut@eldorado.org.br>
2022-03-17 11:47:12 -0300

    done hypertunning methods

--- notebooks/es_utils.py
+++ notebooks/es_utils.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 #from tensorflow.keras.utils import to_categorical
-
+import keras_tuner as kt
 
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
@@ -13,7 +13,9 @@ from sklearn.metrics import classification_report
 #from skmultilearn.model_selection import iterative_train_test_split
 
 import glob
-
+import json
+import random
+from functools import partial
 
 class MLPMultilabel:
     def __init__(self, input_dim, num_classes, neurons_1=16, neurons_2=None, l2=0.01) -> None:
@@ -28,7 +30,7 @@ class MLPMultilabel:
         print(f'Test results - Loss: {test_results[0]} - Accuracy: {test_results[1]}%')
         print(f'Averaged Balanced Accuracy: {ba:.6f}')
         
-        return test_results
+        return test_results, ba
 
     def predict(self, x):
         return self.model.predict(x)
@@ -48,6 +50,111 @@ class MLPMultilabel:
         sgd = tf.keras.optimizers.SGD(learning_rate=0.1, decay=1e-2, momentum=0.5)
         model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['categorical_accuracy'])
         return model
+
+    
+    def hypertunning(self, x_train_norm, y_train, n_hidden=1):
+        #if n_hidden == 2:
+        #    partial_hyper = partial(self.hyper_model_2_build, input_dim=x_train_norm.shape[1], num_classes=y_train.shape[1])
+        #else:
+        partial_hyper = partial(self.hyper_model_build, input_dim=x_train_norm.shape[1], num_classes=y_train.shape[1])
+            #partial_hyper = partial(self.hyper_model_1_build, input_dim=x_train_norm.shape[1], num_classes=y_train.shape[1])
+        tuner = kt.Hyperband(partial_hyper,
+                     objective='val_categorical_accuracy',
+                     max_epochs=10,
+                     factor=3,
+                     directory='hypertunning',
+                     project_name='experimento_1')
+        stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=5)
+
+        tuner.search(x_train_norm, y_train, epochs=50, validation_split=0.2, callbacks=[stop_early])
+        # Get the optimal hyperparameters
+        best_hps=tuner.get_best_hyperparameters(num_trials=1)[0]
+
+        ##print(f"""
+        ##The hyperparameter search is complete. The optimal number of units in the first densely-connected
+        ##layer is {best_hps.get('units_1')} and the optimal learning rate for the optimizer
+        ##is {best_hps.get('learning_rate')}.
+        ##""")
+
+        # Build the model with the optimal hyperparameters and train it on the data for 50 epochs
+        model = tuner.hypermodel.build(best_hps)
+        history = model.fit(x_train_norm, y_train, epochs=50, validation_split=0.2)
+
+        val_acc_per_epoch = history.history['val_categorical_accuracy']
+        best_epoch = val_acc_per_epoch.index(max(val_acc_per_epoch)) + 1
+        print('Best epoch: %d' % (best_epoch,))
+
+        hypermodel = tuner.hypermodel.build(best_hps)
+
+        # Retrain the model
+        hypermodel.fit(x_train_norm, y_train, epochs=best_epoch, batch_size=10, validation_split=0.2)
+
+        self.model = hypermodel
+
+        return hypermodel, best_hps, best_epoch
+
+    
+    def hyper_model_build(self, hp, input_dim, num_classes) -> Sequential:
+        model = Sequential()
+
+        hp_units = hp.Int('units_1', min_value=4, max_value=64, step=4)
+        hp_learning_rate = hp.Choice('learning_rate', values=[1e-1, 1e-2, 1e-3, 1e-4])
+        hp_momentum = hp.Choice('momentum', values=[0.8, 0.5, 0.3, 0.1])
+        hp_l2 = hp.Choice('l2', values=[1e-2, 1e-3, 1e-4])
+
+        model.add(Dense(units=hp_units, input_dim=input_dim, activation='relu', activity_regularizer=tf.keras.regularizers.l2(hp_l2)))
+
+        for i in range(1, hp.Int("num_layers", 1, 2)):
+            model.add(
+                Dense(
+                    units=hp.Int(f"units_{i+1}", min_value=4, max_value=64, step=4),
+                    activation='relu', activity_regularizer=tf.keras.regularizers.l2(hp_l2))
+                )
+
+
+        model.add(Dense(num_classes, activation='softmax'))
+
+        # Configure the model and start training
+        sgd = tf.keras.optimizers.SGD(learning_rate=hp_learning_rate, decay=1e-2, momentum=hp_momentum)
+        model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['categorical_accuracy'])
+        return model
+
+
+    '''def hyper_model_1_build(self, hp, input_dim, num_classes) -> Sequential:
+        model = Sequential()
+
+        hp_units = hp.Int('units', min_value=4, max_value=64, step=4)
+        hp_learning_rate = hp.Choice('learning_rate', values=[1e-1, 1e-2, 1e-3, 1e-4])
+        hp_momentum = hp.Choice('momentum', values=[0.8, 0.5, 0.3, 0.1])
+        hp_l2 = hp.Choice('l2', values=[1e-2, 1e-3, 1e-4])
+
+        model.add(Dense(units=hp_units, input_dim=input_dim, activation='relu', activity_regularizer=tf.keras.regularizers.l2(hp_l2)))
+        model.add(Dense(num_classes, activation='softmax'))
+
+        # Configure the model and start training
+        sgd = tf.keras.optimizers.SGD(learning_rate=hp_learning_rate, decay=1e-2, momentum=hp_momentum)
+        model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['categorical_accuracy'])
+        return model
+    
+    def hyper_model_2_build(self, hp, input_dim, num_classes) -> Sequential:
+        model = Sequential()
+
+        hp_units_1 = hp.Int('units_1', min_value=4, max_value=64, step=4)
+        hp_units_2 = hp.Int('units_2', min_value=4, max_value=64, step=4)
+        hp_learning_rate = hp.Choice('learning_rate', values=[1e-1, 1e-2, 1e-3, 1e-4])
+        hp_momentum = hp.Choice('momentum', values=[0.8, 0.5, 0.3, 0.1])
+        hp_l2 = hp.Choice('l2', values=[1e-2, 1e-3, 1e-4])
+
+        model.add(Dense(units=hp_units_1, input_dim=input_dim, activation='relu', activity_regularizer=tf.keras.regularizers.l2(hp_l2)))
+        model.add(Dense(hp_units_2, activation='relu'))
+        model.add(Dense(num_classes, activation='softmax'))
+
+        # Configure the model and start training
+        sgd = tf.keras.optimizers.SGD(learning_rate=hp_learning_rate, decay=1e-2, momentum=hp_momentum)
+        model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['categorical_accuracy'])
+        return model'''
+
+    
         
 
 class DataProcessingExtrasensory:
@@ -96,6 +203,8 @@ class HAR:
         self.config = {
             'df_path': None,
             'df': None,
+            'hypertunning': False,
+            'hypertunning_params': {},
             'neurons_1' : 16, 
             'neurons_2' : None, 
             'l2' : 0.01,
@@ -110,12 +219,17 @@ class HAR:
             self.data= DataProcessingExtrasensory(self.load(self.config['df_path']), labels=self.config['labels']) #TODO: attention
         else:
             self.data= DataProcessingExtrasensory(self.config['df'], labels=self.config['labels']) #TODO: attention
-        self.mlp = self.make_mlp(
-            self.data.x_train.shape[1], 
-            self.data.y_train.shape[1], 
-            neurons_1=self.config['neurons_1'], 
-            neurons_2=self.config['neurons_2'], 
-            l2=self.config['l2'])
+        
+        if not self.config['hypertunning']:
+            self.mlp = self.make_mlp(
+                self.data.x_train.shape[1], 
+                self.data.y_train.shape[1], 
+                neurons_1=self.config['neurons_1'], 
+                neurons_2=self.config['neurons_2'], 
+                l2=self.config['l2'])
+        
+        else:
+            self.hypertunning()
 
 
 
@@ -129,9 +243,32 @@ class HAR:
         #return pd.read_csv('../input/user1.features_labels.csv')#.dropna()
         return pd.read_csv(df_path)
 
-    def dummy_test(self):
+    def run(self):
         self.mlp.train(self.data.x_train, self.data.y_train)
         self.mlp.evaluate(self.data.x_test, self.data.y_test)
+
+    #TODO: test
+    def hypertunning(self):
+        results = {}
+        '''model, best_hps, best_epoch = self.mlp.hypertunning(self.data.x_train, self.data.y_train)
+        test_results, ba = self.mlp.evaluate(self.data.x_test, self.data.y_test)
+        results['1_hidden'] = [model, best_hps, best_epoch, test_results, ba]
+
+        model, best_hps, best_epoch = self.mlp.hypertunning(self.data.x_train, self.data.y_train, n_hidden=2)
+        test_results, ba = self.mlp.evaluate(self.data.x_test, self.data.y_test)
+        results['2_hidden'] = [model, best_hps, best_epoch, test_results, ba]
+
+        if results['1_hidden'][4] > results['2_hidden'][4]: #1 hidden layer with better BA
+            model, best_hps, best_epoch, test_results, ba = results['1_hidden']
+            self.mlp.model = model
+        '''
+
+        model, best_hps, best_epoch = self.mlp.hypertunning(self.data.x_train, self.data.y_train)
+        test_results, ba = self.mlp.evaluate(self.data.x_test, self.data.y_test)
+        #results['1_hidden'] = [model, best_hps, best_epoch, test_results, ba]
+
+
+        return model, best_hps, best_epoch, test_results, ba
         
     def evaluate(self):
         self.mlp.evaluate(self.data.x_test, self.data.y_test)
@@ -156,17 +293,41 @@ def testar():
 
 
 def get_all_user_csvs(folderpath : str):
-    for filename in glob.iglob(f'{folderpath}/**', recursive=True):
-        print(filename)
-        filename.split(folderpath)[1].split('.features')
-        #with open(filename, 'r+') as json_data:  # abrir o fields
-        #    results.append(json.load(json_data))
+    answer = []
+    for filename in glob.iglob(f'{folderpath}/**.csv', recursive=True):
+        answer.append(filename)
+    return answer
 
 
-def create_k_folds_n_users():
-    pass
+def create_k_folds_n_users(k_folds: int, n_users: int, folderpath: str):
+    all_csvs = get_all_user_csvs(folderpath)
+    all_dfs = {}
+    for i in range(k_folds):
+        fold_list_train = random.sample(all_csvs, n_users)
+        fold_df_train = pd.read_csv(fold_list_train[0])
+        for csv in fold_list_train[1:]:
+            fold_df_train = fold_df_train.append(pd.read_csv(csv))
+
+        fold_list_test = np.setdiff1d(all_csvs, fold_list_train)
+        #fold_df_test = pd.read_csv(fold_list_test[0])
+        #for csv in fold_list_test[1:]:
+        fold_list_test = [csv.split(f'{folderpath}/')[1] for csv in fold_list_test]
+
+            #fold_df_test = fold_df_test.append(pd.read_csv(csv))
+        #print(fold_list_test)
+        with open(f'fold_{i}_test_cvs.json', 'w+') as json_data:  # abrir o fields
+            json.dump(fold_list_test, json_data)
+        #    results.append(json.load(json_data)) 
+        #pessima ideia --> muito espaço em disco
+        #fold_df_train.to_csv(f'fold_{i}_train.csv')
+        #fold_df_test.to_csv(f'fold_{i}_test.csv')
+        all_dfs[f'fold_{i}'] = {'train': fold_df_train}#, 'test': fold_df_test}
+
+    return all_dfs
 
 if __name__ == '__main__':
     pass
-    get_all_user_ids('/home/wander/OtherProjects/har_flower/sample_data')
+    a = create_k_folds_n_users(5, 3, '/home/wander/OtherProjects/har_flower/sample_data')
+    #a = create_k_folds_n_users(5, 40, '/home/wander/OtherProjects/har_flower/full_data')
+    #print(a['fold_0'])
     #testar()
