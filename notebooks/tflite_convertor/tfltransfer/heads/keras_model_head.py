5b8d0b99d398c5a601995a646cd550adf829031e
afreireosorio <alexandreosorio@yahoo.com>
2022-03-18 15:22:36 -0300

    Usando tf.keras.models.save_model, a seguinte msg de erro eh exibida: RuntimeError: MetaGraphDef associated with tags 'eval' could not be found in SavedModel, with available tags '[{'serve'}]'.

--- notebooks/tflite_convertor/tfltransfer/heads/keras_model_head.py
+++ notebooks/tflite_convertor/tfltransfer/heads/keras_model_head.py
@@ -42,8 +42,7 @@ class KerasModelHead(object):
     def __init__(self, keras_model):
         # Convert Keras model to SavedModel.
         saved_model_dir = tempfile.mkdtemp("tflite-transfer-keras-model")
-        tf.compat.v1.keras.models.save_model(keras_model, saved_model_dir)
-#        tf.compat.v1.keras.experimental.export_saved_model(keras_model, saved_model_dir)
+        tf.compat.v1.keras.experimental.export_saved_model(keras_model, saved_model_dir)
 
         # Pre-fetch some information about the model.
         with tfv1.Session(graph=tf.Graph()) as sess:
