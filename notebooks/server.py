ef2265a92deb911c104c58a9f93785f714bd7d5a
Wandemberg Gibaut <wandemberg.gibaut@eldorado.org.br>
2022-04-19 12:20:25 -0300

    experiments done

--- notebooks/server.py
+++ notebooks/server.py
@@ -9,9 +9,9 @@ def main() -> None:
     strategy = fl.server.strategy.FedAvg(
         fraction_fit=1.0,
         fraction_eval=1.0,
-        min_fit_clients=2,
-        min_eval_clients=2,
-        min_available_clients=2,
+        min_fit_clients=6,
+        min_eval_clients=12,
+        min_available_clients=12,
         #eval_fn=None,
         on_fit_config_fn=fit_config,
         #on_evaluate_config_fn=evaluate_config,
@@ -19,7 +19,7 @@ def main() -> None:
     )
 
     # Start Flower server for 10 rounds of federated learning
-    fl.server.start_server("[::]:8080", config={"num_rounds": 20}, strategy=strategy, force_final_distributed_eval=True)
+    fl.server.start_server("[::]:8080", config={"num_rounds": 40}, strategy=strategy, force_final_distributed_eval=True)
 
 
 def fit_config(rnd: int):
@@ -29,7 +29,7 @@ def fit_config(rnd: int):
     """
     config = {
         "batch_size": 50,
-        "local_epochs": 1,
+        "local_epochs": 3,
     }
     return config
 
